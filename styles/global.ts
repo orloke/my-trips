c234f468df9efeb025a16a223049b01bd741788a
Junior Dering <juniordering@hotmail.com>
2022-10-23 16:06:52 -0400

    variable css

--- styles/global.ts
+++ styles/global.ts
@@ -1,6 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  :root{
+    --highlight: #e20e8d;
+    --background: #030518;
+    --white: #eeeeee;
+    
+    --container: 100rem;
+
+    --small: 1.5rem;
+    --medium: 3rem;
+    --larger: 5rem;
+  }
   *{
     margin: 0;
     padding: 0;
@@ -9,6 +20,8 @@ export const GlobalStyles = createGlobalStyle`
 
   html, body, #__next{
     height: 100%;
+    background: var(--background);
+    color: var(--white);
   }
 
   @media (max-width: 1068px) {
@@ -24,5 +37,15 @@ export const GlobalStyles = createGlobalStyle`
 
     body{
       font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
+    p, a{
+      font-size: 2rem;
+      line-height: var(--medium);
+    }
+
+    a{
+      text-decoration: none;
+      color: var(--highlight);
     }
 `;
