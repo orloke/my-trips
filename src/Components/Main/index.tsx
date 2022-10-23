eee9ad77fa409ed3e3dd4dba68120da8a4f4d662
Junior Dering <juniordering@hotmail.com>
2022-10-23 15:54:03 -0400

    starting projec my trips

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -1,16 +1,5 @@
-import * as S from './styles';
-
 export const Main = () => (
-  <S.Wrapper>
-    <S.Logo
-      src="img/logo.svg"
-      alt="Imagem de um atomo com React Avançado escrito ao lado"
-    />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>TypeScript, React, NextJS e Styled Components</S.Description>
-    <S.Ilustration
-      src="img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente de um tela com código"
-    />
-  </S.Wrapper>
+  <div>
+    <h1>oi a todos</h1>
+  </div>
 );
